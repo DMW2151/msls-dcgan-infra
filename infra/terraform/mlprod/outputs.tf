ff2116f6cf69c777506862c0684e3d9380d08221
Dustin Wilson <dmw2151@columbia.edu>
2022-01-31 21:43:51 -0500

    updates; weekend 02

--- infra/terraform/mlprod/outputs.tf
+++ infra/terraform/mlprod/outputs.tf
@@ -3,10 +3,10 @@ output "jump_ip_addr" {
   value       = module.mlcore.jump_ip_addr
 }
 
-# output "metrics_ip_addr" {
-#   description = "Internal IP address of the VPC's metrics instance. Deployer can SSH tunnel into this instance via jump."
-#   value       = module.stats-monitor.metrics_ip_addr
-# }
+output "metrics_ip_addr" {
+  description = "Internal IP address of the VPC's metrics instance. Deployer can SSH tunnel into this instance via jump."
+  value       = module.stats-monitor.metrics_ip_addr
+}
 
 output "worker_ip_addr" {
   description = "Internal IP address of the VPC's worker instance. Deployer can SSH tunnel into this instance via jump."
